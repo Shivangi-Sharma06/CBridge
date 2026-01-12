ec2f7dcce4ccf5d9353c3bbd96bbdab691847c9a
Shivangi-Sharma06 <shivangi18112006@gmail.com>
2026-01-13 00:13:16 +0530

    added mockingsol

--- cBridge.sol
+++ cBridge.sol
@@ -1,3 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// four functions - stake, addreward, unstake, deductslash
+// exchange arte api needed / what if total share =0
+
+
+
+contract MockSol{
+    
+    uint totalShares;
+    uint shares;
+    uint totalETH;
+
+    function stake () external payable{
+            uint amount = msg.value; //recieve 
+            address user= msg.sender; //total eth
+
+            if(totalShares==0){
+                shares = totalETH;
+            }
+            else{
+                shares = totalETH + amount;
+            }
+
+
+
+
+    }
+
+
+}
